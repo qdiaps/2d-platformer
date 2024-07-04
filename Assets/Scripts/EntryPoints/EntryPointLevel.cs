143c88d3a74d82151a37091c7a8e83ddb15a7b57
qdiaps <145145693+qdiaps@users.noreply.github.com>
2024-07-04 15:43:26 +0300

    Create 1 game level (static)

--- Assets/Scripts/EntryPoints/EntryPointLevel.cs
+++ Assets/Scripts/EntryPoints/EntryPointLevel.cs
@@ -5,16 +5,24 @@ namespace EntryPoints
     public class EntryPointLevel : EntryPoint
     {
         [SerializeField] private GameObject _prefabPlayer;
+        [SerializeField] private GameObject[] _prefabsStatic;
 
         protected override void Start()
         {
             base.Start();
             CreatePlayer();
+            CreateStaticObjects();
         }
 
         private void CreatePlayer()
         {
             Instantiate(_prefabPlayer);
+        }
+
+        private void CreateStaticObjects()
+        {
+            foreach (GameObject prefab in _prefabsStatic)
+                Instantiate(prefab);
         }
     }
 }
